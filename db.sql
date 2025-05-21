76fb314b7277181c10b47e93f6ba7947aa1ab133
nhan295 <tranthihongnhan295@gmail.com>
2025-05-21 15:38:56 +0700

    refactor(db): add query to see search product function

--- db.sql
+++ db.sql
@@ -82,18 +82,21 @@ create table color_code(
 );
 select * from color_code;
 drop table color_code;
-insert into color_code(jewelry_id,color_name) values(1,'red');
+insert into color_code(jewelry_id,color_name) values(1,'blue');
 create table size(
 	size_id int primary key auto_increment,
     size_number int unique,
     jewelry_id int,
+    color_id int,
     quantity int,
     
-    foreign key(jewelry_id) references jewelry(jewelry_id)
+    foreign key(jewelry_id) references jewelry(jewelry_id),
+    foreign key(color_code) references color_code(color_id)
 );
 select * from size;
-insert into size(size_number,jewelry_id,quantity) values(1,1,1);
-insert into size(size_number,jewelry_id,quantity) value(20,1,5);
+insert into size(size_number,jewelry_id,quantity,color_id) values(45,1,1,3);
+insert into size(size_number,jewelry_id,quantity,color_id) value(50,1,4,4);
+
 
 create table jewelry_img(
 	img_id int primary key auto_increment,
@@ -105,8 +108,9 @@ create table jewelry_img(
     foreign key (jewelry_id) references jewelry(jewelry_id)
 
 );
-insert into jewelry_img(image,color_id,jewelry_id) values('./src/assets/image/vongbac.jpg',1,1)
-
+insert into jewelry_img(image,color_id,jewelry_id) values('./src/assets/image/vongbac.jpg',3,1);
+truncate table jewelry_img;
+select * from jewelry_img;
 create table cart(
 	cart_id int primary key auto_increment,
     user_id int,
@@ -164,6 +168,25 @@ FROM jewelry j
 JOIN color_code  ON color_code.jewelry_id = j.jewelry_id
 JOIN size ON size.jewelry_id = j.jewelry_id
 WHERE jewelry_name LIKE '%pandora bac%';
+
+
+SELECT 
+  jewelry.jewelry_name,
+  jewelry.jewelry_price, 
+  color_code.color_name,
+  size.size_number,
+  jewelry_img.image
+FROM jewelry
+INNER JOIN size ON size.jewelry_id = jewelry.jewelry_id
+LEFT JOIN color_code 
+  ON color_code.jewelry_id = jewelry.jewelry_id 
+  AND color_code.color_id = size.color_id
+LEFT JOIN jewelry_img 
+  ON jewelry_img.jewelry_id = jewelry.jewelry_id 
+  AND jewelry_img.color_id = color_code.color_id
+WHERE jewelry_name LIKE '%pandora bac%'
+GROUP BY jewelry.jewelry_id, color_code.color_id
+LIMIT 4;
 
 -- get jewelry detail-------------------------
 SELECT 
